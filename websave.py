c638c4287e41da6cc79e13a6466344bf9b29aec1
Ross Mawhorter <rmawhorter@g.hmc.edu>
2020-04-06 13:01:50 -0700

    Added user header and logging to a csv for failures.

--- websave.py
+++ websave.py
@@ -10,6 +10,8 @@ from datetime import datetime
 import argparse
 import ssl
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
 def get_urls(csv_fname):
     l = []
     with open(csv_fname, newline="") as csvfile:
@@ -23,17 +25,21 @@ def save_webpage(url, fname):
     try:
         # Read the page
         s = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        req = urllib.request.Request(url, headers=headers)
         html = urlopen(url, context=s)
         page = html.read()
         # Save it
         os.makedirs(os.path.dirname(fname), exist_ok=True)
         with open(fname, "wb") as f:
             f.write(page)
+        return True
     except:
         print("The URL {} could not be opened".format(url))
+        return False
 
-def save_all(csv_fname, folder):
+def save_all(csv_fname, folder, fails_csv):
     urls = get_urls(csv_fname)
+    fails = []
     for university, category, u in urls:
         print("URL: {}".format(u))
         # Make the url safe for a filename
@@ -47,12 +53,18 @@ def save_all(csv_fname, folder):
         time_stamp = time_stamp.replace(":", "-")
         f = university + "_" + category + "_" + f + time_stamp + ".html"
         fname = os.path.join(folder, university, f)
-        save_webpage(u, fname)
+        saved = save_webpage(u, fname)
+        if not saved:
+            fails.append((u,f))
+    with open(fails_csv, "w", newline="") as csvfile:
+        w = csv.writer(csvfile)
+        for u,f in fails:
+            w.writerow([u,f])
 
-def save_all_loop(csv_fname, folder, period):
+def save_all_loop(csv_fname, folder, period, fails):
     while True:
         print("Saving URLs")
-        save_all(csv_fname, folder)
+        save_all(csv_fname, folder, fails)
         time.sleep(period)
         print("Done Saving")
         print("Waiting for {} seconds".format(period))
@@ -61,6 +73,7 @@ def get_args(arg_list):
     parser = argparse.ArgumentParser()
     parser.add_argument("--folder", metavar="<folder>", required=True, help="The path to the folder to save the .html files.")
     parser.add_argument("--csv", metavar="<filename>", required=True, help="The path to the csv file to read the URLs from.")
+    parser.add_argument("--fails", metavar="<filename>", required=False, default="fails.csv")
     parser.add_argument("--period", metavar="n", required=False, const=86400, type=int, nargs="?", help="How often to save (in seconds). Defaults to 1 day = 86400 seconds")
     args = parser.parse_args(arg_list)
     return args
@@ -69,9 +82,9 @@ def main(arg_list):
     args = get_args(arg_list)
     # If periodic, then run the loop
     if args.period is not None:
-        save_all_loop(args.csv, args.folder, args.period)
+        save_all_loop(args.csv, args.folder, args.period, args.fails)
     else:
-        save_all(args.csv, args.folder)
+        save_all(args.csv, args.folder, args.fails)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
